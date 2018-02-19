b42be0138331b9768846c9de4b90c7b0e5e4deba
CC <martinchukachinaka@gmail.com>
2018-02-19 13:07:02 +0100

    Task updates

--- src/main/java/com/cc/engagetech/expenses/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cc.engagetech.expenses.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cc.engagetech.expenses.model.Role;
-
-
-public interface RoleRepository extends JpaRepository<Role, String> {
-}
--- src/main/java/com/cc/engagetech/expenses/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cc.engagetech.expenses.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cc.engagetech.expenses.model.User;
-
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
